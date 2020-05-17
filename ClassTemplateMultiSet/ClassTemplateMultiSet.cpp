37815a6e4263403f5866d01b7e5af5c0fff01eb3
mateieboss <mateimindruleanu@gmail.com>
2020-05-17 14:56:52 +0300

    Am renuntat la AVL :(

--- ClassTemplateMultiSet/ClassTemplateMultiSet.cpp
+++ ClassTemplateMultiSet/ClassTemplateMultiSet.cpp
@@ -7,6 +7,21 @@ int main()
     M.insert(1);
     M.insert(33);
     M.insert(0);
+    M.insert(22);
+    M.insert(69);
+    MultiSet<int> N = M;
     cout << M;
+    M.remove(69);
+    cout << M;
+    M.remove(1);
+    cout << M;
+    M.remove(22);
+    cout << M;
+    cout << N;
+    N = M;
+    cout << N;
+    N.remove(0);
+    cout << N;
     return 0;
+
 }
--- ClassTemplateMultiSet/MultiSet.h
+++ ClassTemplateMultiSet/MultiSet.h
@@ -9,19 +9,26 @@ template<typename T, typename F = Comparator<T> >
 class MultiSet
 {
 	Node<T>* root;
+
+	void copy(Node<T>*);
 public:
 	MultiSet();
+	MultiSet(const MultiSet&);
 
 	void insert(T);
+	void remove(T);
+	void dilit(Node<T>*);
 	void SRD(ostream&, Node<T>*);
 	T getRoot()
 	{
 		return this->root->getInfo();
 	}
 
+	MultiSet& operator=(const MultiSet&);
 	friend ostream& operator<<(ostream& out, MultiSet<T, F> &M)
 	{
 		M.SRD(out,M.root);
+		out << endl;
 		return out;
 	}
 };
@@ -30,6 +37,12 @@ template<typename T, typename F>
 MultiSet<T, F>::MultiSet()
 {
 	root = nullptr;
+}
+
+template<typename T, typename F>
+MultiSet<T, F>::MultiSet(const MultiSet& S)
+{
+	copy(S.root);
 }
 
 template<typename T, typename F>
@@ -46,7 +59,7 @@ void MultiSet<T, F>::insert(T x)
 		while (W)
 		{
 			aux = W;
-			if (F::less(x, W->getInfo()) || F::equals(x,W->getInfo()) )
+			if  ( F::less(x, W->getInfo()) || F::equals(x,W->getInfo()) )
 			{
 				W = W->getLeft();
 			}
@@ -77,3 +90,96 @@ void MultiSet<T, F>::SRD(ostream& out, Node<T>* N)
 		SRD(out,N->getRight());
 	}
 }
+
+template<typename T, typename F>
+MultiSet<T, F>& MultiSet<T, F>::operator=(const MultiSet& N)
+{
+	if (this == &N) return *this;
+	dilit(root);
+	root = nullptr;
+	copy(N.root);
+	return *this;
+}
+
+template<typename T, typename F>
+void MultiSet<T, F>::dilit(Node<T>* N)
+{
+	if (N)
+	{
+		dilit(N->getLeft());
+		dilit(N->getRight());
+		delete N;
+	}
+}
+
+template<typename T, typename F>
+void MultiSet<T, F>::copy(Node<T>* N)
+{
+	if (N)
+	{
+		insert(N->getInfo());
+		copy(N->getLeft());
+		copy(N->getRight());
+	}
+}
+
+template<typename T, typename F>
+void MultiSet<T, F>::remove(T x)
+{
+	Node<T>* W = root;
+	Node<T>* aux = W;
+	while (W)
+	{
+		if (F::greater(W->getInfo(), x))
+		{
+			aux = W;
+			W = W->getLeft();
+		}
+		else if (F::less(W->getInfo(), x))
+		{
+			aux = W;
+			W = W->getRight();
+		}
+		else
+		{
+			if (!W->getLeft() && !W->getRight())
+			{
+				if (aux->getLeft() == W) {
+					aux->setLeft(nullptr);
+				}
+				else {
+					aux->setRight(nullptr);
+				}
+				delete W;
+			}
+			else if (W->getLeft() && !W->getRight())
+			{
+				if (aux->getLeft() == W)
+					aux->setLeft(W->getLeft());
+				else
+					aux->setRight(W->getLeft());
+				delete W;
+			}
+			else if (!W->getLeft() && W->getRight())
+			{
+				if (aux->getLeft() == W)
+					aux->setLeft(W->getRight());
+				else
+					aux->setRight(W->getRight());
+				delete W;
+			}
+			else
+			{
+				Node<T>* help = W->getRight();
+				while (help->getLeft())
+					help = help->getLeft();
+				T v = help->getInfo();
+				remove(help->getInfo());
+				W->setInfo(v);
+			}
+			return;
+		}
+	}
+	return;
+}
+
